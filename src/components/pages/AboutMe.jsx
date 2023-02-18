94c2b14d75957544ae3f4217a17c53cff4d3118c
Eren <erencan_inanci@hotmail.com>
2023-02-19 01:22:07 +0300

    build update

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -8,17 +8,9 @@ export default function AboutMe() {
         <h1 className="hello">Hello!</h1>
         <div className="me">
           <p className="text">
-            I am a full stack web developer with a background in mechanical
-            engineering. I have a passion for learning and creating. I am
-            currently looking for a full time position as a web developer.{" "}
-            <br></br>
-            <br></br>I am a full stack web developer with a background in
-            mechanical engineering. I have a passion for learning and creating.
-            I am currently looking for a full time position as a web developer.
-            <br></br>
-            <br></br>I am a full stack web developer with a background in
-            mechanical engineering. I have a passion for learning and creating.
-            I am currently looking for a full time position as a web developer.
+            I am a full stack developer with a background in CS degree. I have a
+            passion for learning and creating. I am currently looking for a full
+            time position as a Game Developer or Web Developer.
           </p>
           <img
             className="picture"
