4b414a07319d42eabe461581e5bad9e6cd2475c1
Eren <erencan_inanci@hotmail.com>
2023-02-19 04:27:21 +0300

    update

--- src/components/views/ContactItem.jsx
+++ src/components/views/ContactItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../../styles/project-items/ContactItem.css";
 
 export default function ContactItem(props) {
   return (
-    <div>
+    <div className="contact-item">
       <img src={props.icon} alt="" />
       <a href={props.url}>{props.url}</a>
     </div>
